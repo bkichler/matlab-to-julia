3349539f1288bf06d492163bae4b7dfb54e44fab
Brian Kichler <brian.kichler@gmail.com>
2023-07-12 23:17:02 +0000

    Switch to lightweight server

--- index.js
+++
@@ -1,8 +0,0 @@
-// Importing the express module
-const express = require('express');
-const app = express();
-
-// Setting up the server at port 3000
-app.listen(8080, ()=>{
-   console.log("server running");
-});
